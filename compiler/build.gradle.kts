ratanasoth/kotlin

Convert more projects

compiler/build.gradle.kts
@@ -6,6 +6,10 @@ apply { plugin("kotlin") }
 
 jvmTarget = "1.6"
 
+configureIntellijPlugin {
+    setExtraDependencies("intellij-core")
+}
+
 val compilerModules: Array<String> by rootProject.extra
 val otherCompilerModules = compilerModules.filter { it != path }
 
@@ -50,9 +54,14 @@ dependencies {
     otherCompilerModules.forEach {
         testCompileOnly(project(it))
     }
-    testCompile(ideaSdkDeps("openapi", "idea", "util", "asm-all", "commons-httpclient-3.1-patched"))
-    testRuntime(ideaSdkCoreDeps("*.jar"))
-    testRuntime(ideaSdkDeps("*.jar"))
+}
+
+afterEvaluate {
+    dependencies {
+        testCompileOnly(intellij { include("openapi.jar", "idea.jar", "util.jar", "asm-all.jar", "commons-httpclient-3.1-patched.jar") })
+        testRuntime(intellijCoreJar())
+        testRuntime(intellij())
+    }
 }
 
 sourceSets {

magazos/kotlin-1

compiler/build.gradle.kts
@@ -6,6 +6,10 @@ apply { plugin("kotlin") }
 
 jvmTarget = "1.6"
 
+configureIntellijPlugin {
+    setExtraDependencies("intellij-core")
+}
+
 val compilerModules: Array<String> by rootProject.extra
 val otherCompilerModules = compilerModules.filter { it != path }
 
@@ -52,16 +56,21 @@ dependencies {
     otherCompilerModules.forEach {
         testCompileOnly(project(it))
     }
-    testCompile(ideaSdkDeps("openapi", "idea", "util", "asm-all", "commons-httpclient-3.1-patched"))
 
     testRuntime(projectDist(":kotlin-compiler"))
     testRuntime(projectDist(":kotlin-daemon-client"))
     testRuntime(preloadedDeps("dx", subdir = "android-5.0/lib"))
-    testRuntime(ideaSdkCoreDeps("*.jar"))
-    testRuntime(ideaSdkDeps("*.jar"))
     testRuntime(files("${System.getProperty("java.home")}/../lib/tools.jar"))
 
     testJvm6ServerRuntime(projectTests(":compiler:tests-common-jvm6"))
+}
+
+afterEvaluate {
+    dependencies {
+        testCompile(intellij { include("openapi.jar", "idea.jar", "util.jar", "asm-all.jar", "commons-httpclient-3.1-patched.jar") })
+        testRuntime(intellijCoreJar())
+        testRuntime(intellij())
+    }
 }
 
 sourceSets {